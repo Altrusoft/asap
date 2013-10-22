fb09b225aa9deb4777301352d4ef733cfda4f84d
hanshook <hans.hook@altrusoft.se>
2013-10-22 18:26:12 +0200

    Added children and url to content

--- source/java/se/altrusoft/alfplay/node/NodeMetadataGet.java
+++ source/java/se/altrusoft/alfplay/node/NodeMetadataGet.java
@@ -22,9 +22,11 @@ package se.altrusoft.alfplay.node;
   
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.repository.StoreRef;
@@ -38,6 +40,7 @@ import org.springframework.extensions.webscripts.WebScriptRequest;
 import org.springframework.extensions.webscripts.WebScriptResponse;
 
 import se.altrusoft.alfplay.repo.dictionary.DictionaryServiceExtras;
+import se.altrusoft.alfplay.repo.dictionary.SimpleQName;
 
 public class NodeMetadataGet extends AbstractWebScript {
 
@@ -72,10 +75,14 @@ public class NodeMetadataGet extends AbstractWebScript {
 		QName nodeType = this.nodeService.getType(nodeRef);
 		Set<QName> aspects = this.nodeService.getAspects(nodeRef);
 		Map<QName, Serializable> props = this.nodeService.getProperties(nodeRef);
-			
-	
+		List<ChildAssociationRef> children = this.nodeService.getChildAssocs(nodeRef);
+
+		// The API for downloading content is: /api/node/content{property}/{store_type}/{store_id}/{id}
+		// example:
+		// http://localhost:8080/alfresco/service/api/node/content/workspace/SpacesStore/6ce33a7b-6c99-4dbb-978f-4ae3c6af6421
 		JSONObject jsonResult = new JSONObject();
-		
+		String store_type = "";
+		String store_id = "";
 		try {
 			JSONArray jsonAspects = new JSONArray();
 			for (QName aspect: aspects) {
@@ -84,13 +91,31 @@ public class NodeMetadataGet extends AbstractWebScript {
 			JSONArray jsonProps = new JSONArray();
 			for (Map.Entry<QName, Serializable> prop: props.entrySet()) {
 				JSONObject jsonProperty = new JSONObject();
-				jsonProperty.put(this.dictionaryServiceExtras.getSimpleQName(prop.getKey()).toString(), prop.getValue());
+				SimpleQName simpleQname = this.dictionaryServiceExtras.getSimpleQName(prop.getKey());
+				if (simpleQname.getPrefix().equalsIgnoreCase("sys")) {
+					if (simpleQname.getShortName().equalsIgnoreCase("store-identifier")) {
+						store_id=prop.getValue().toString();
+					}
+					else if (simpleQname.getShortName().equalsIgnoreCase("store-protocol")) {
+						store_type=prop.getValue().toString();
+					}
+				}
+				jsonProperty.put(simpleQname.toString(), prop.getValue());
 				jsonProps.put(jsonProperty);
 			}
+			JSONArray jsonChildren = new JSONArray();
+			for (ChildAssociationRef child: children) {
+				JSONObject jsonProperty = new JSONObject();
+				jsonProperty.put(this.dictionaryServiceExtras.getSimpleQName(child.getTypeQName()).toString(), child.getChildRef().getId());	
+				jsonChildren.put(jsonProperty);
+			}
+			
 			jsonResult.put("nodeId", nodeId);
 			jsonResult.put("type", this.dictionaryServiceExtras.getSimpleQName(nodeType));
 			jsonResult.put("aspects", jsonAspects);
 			jsonResult.put("properties", jsonProps);
+			jsonResult.put("children", jsonChildren);
+			jsonResult.put("content_url", req.getServiceContextPath()+"/api/node/content/"+store_type+"/"+store_id+"/"+nodeId);
 			String jsonString = jsonResult.toString(4);
 	    	res.getWriter().write(jsonString);
 		}
